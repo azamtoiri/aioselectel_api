43381eda380714f9a5a6df6c1480369b95042f71
spike <azamtoiri@gmail.com>
2024-05-30 22:51:11 +0300

    new version 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='selectel_async',
-    version='0.1.1',
+    version='0.1.4',
     packages=find_packages(),
     author="Azam Toiri",
     author_email="<azamtoiri@gmail.com>",
